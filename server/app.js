b49a5ab5fb43a7c73fb8d580014c0dbfc072f159
Kiruthinavi97 <kivikiruthika97@gmail.com>
2026-03-09 22:45:35 +0530

    CORS updated

--- server/app.js
+++ server/app.js
@@ -13,9 +13,6 @@ dotenv.config()
 const app = express()
 export const myCache = new NodeCache();
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: [
         "http://localhost:5173",
@@ -23,6 +20,10 @@ app.use(cors({
     ],
     credentials: true
 }))
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use("/api/v1/user", userRoutes)
 app.use("/api/v1/course", courseRoutes)
 app.use("/api/v1/payments", paymentRoutes)
