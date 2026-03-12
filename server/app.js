a7ed5e18ca64d609b7ddef75d94987b7fa856aec
Kiruthinavi97 <kivikiruthika97@gmail.com>
2026-03-12 10:54:02 +0530

    updated CORS

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'   
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
 import courseRoutes from './routes/courseRoutes.js'
@@ -12,7 +13,13 @@ dotenv.config()
 const app = express()
 export const myCache = new NodeCache();
 
-
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://lmsguga.netlify.app"
+    ],
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
