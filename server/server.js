0d65d72b821c04c5448972611e6a6eeedc744395
Kiruthinavi97 <kivikiruthika97@gmail.com>
2026-04-20 11:13:13 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -3,16 +3,13 @@ import { connectDb } from './database/db.js'
 import { v2 as cloudinary } from 'cloudinary';
 
 
+
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
 });
 
-export const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_API_KEY,
-    key_secret: process.env.RAZORPAY_API_SECRET
-})
 
 app.use("/test", (req, res) => {
     res.json({ cors: app._router ? "router exists" : "no router", working: true })
